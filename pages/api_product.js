e447b75e9592c35a8e7e71d6e56496507b0c0abd
ChenYiChu <stitch000000@gmail.com>
2021-06-09 00:19:14 +0800

    set api_project

---
+++ pages/api_product.js
@@ -0,0 +1,13 @@
+import  Layout from '../components/layout/Layout';
+export default function api_product() {
+    return (
+        <>
+        <Layout>
+        
+        <h2>Api Products</h2>
+    
+        </Layout>
+        </>
+        
+    )
+}
